96b356dc0e24aef56eb067c8afb5cacc4f2d04a3
Syefoyo <trisyefolionel@gmail.com>
2024-06-23 21:04:03 +0700

    Gues the pin files

---
+++ script.js
@@ -0,0 +1,9 @@
+function bro() {
+      let myInput = document.getElementById("myInput").value;
+      if (myInput === '2178') {
+  alert("correct");
+      }
+      else {
+      alert("Wrong PIN");
+      }
+}
